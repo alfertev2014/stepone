26806a513d1b313a023bbd6e9f0951a44a8e3c92
vasya <alfertev2012@gmail.com>
2014-11-29 14:27:55 +0300

    Небольшой рефакторинг Ob::Ptr.

--- src/core.h
+++ src/core.h
@@ -40,34 +40,32 @@ class Ob {
 public: // static
     class Ptr {
         Ob * ob;
+        inline static void incRefCount(Ob * ob) {if(ob != 0) ob->refcount++; else {DBG("ob == 0"); throw SemanticError();}}
+        inline static void decRefCount(Ob * ob) {if(ob == 0) return; ob->refcount--; if(ob->refcount == 0) delete ob;}
     public:
-        Ptr() : ob(Ob::anil.ob) {if(ob != 0) ob->refcount++;}
-        Ptr(Ob * _ob) : ob(_ob) {if(ob != 0) ob->refcount++;}
-        Ptr(const Ob::Ptr & _ob) : ob(_ob.ob) {if(ob != 0) ob->refcount++; else {DBG("ob == 0"); throw SemanticError();}}
-        ~Ptr() {if(ob == 0) return; ob->refcount--; if(ob->refcount == 0) delete ob;}
+        Ptr() : ob(Ob::anil.ob) {incRefCount(ob);}
+        Ptr(Ob * _ob) : ob(_ob) {incRefCount(ob);}
+        Ptr(const Ob::Ptr & _ob) : ob(_ob.ob) {incRefCount(ob);}
+        Ptr(bool b) : ob(b ? Ob::at.ob : Ob::anil.ob) {incRefCount(ob);}
+        ~Ptr() {decRefCount(ob);}
 
         bool operator == (const Ptr & p) const {return p.ob == ob;}
         bool operator ==(const Ob * const p) const {return ob == p;}
         friend bool operator ==(const Ob * const ob, const Ptr & p);
 
         Ptr & operator=(const Ptr & p) {return this->operator=(p.ob);}
-
         Ptr & operator=(Ob * _ob) {
-            if(_ob == ob) return *this;
-            if(_ob != 0)
-                _ob->refcount++;
-            if(ob != 0) {
-                ob->refcount--;
-                if(ob->refcount == 0)
-                    delete ob;
+            if(_ob != ob) {
+                // don't change order
+                incRefCount(_ob);
+                decRefCount(ob);
+                ob = _ob;
             }
-            ob = _ob;
             return *this;
         }
 
         Ob * operator -> () const {return ob;}
-
-        Ob * getPointer() { return ob;}
+        Ob * getPointer() const {return ob;}
     };
 
     static const Ptr anil;
