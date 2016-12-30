507ced6a4ae054741a03ab2eac74548587c94245
Василий Алфертьев <vasiliy.alfertev@osinit.ru>
2016-12-30 17:46:23 +0400

    Revert "Remove CurrentContext type."
    This reverts commit f7476d63f7b22ebc5b721f46959b3ed7d1bbedc6.

--- src/core/basemacro.cpp
+++ src/core/basemacro.cpp
@@ -42,7 +42,7 @@ Ptr MMacro::apply(const Ptr &p, const Ptr &a) {
 }
 
 Ptr MCurrentContext::apply(const Ptr &p, const Ptr &a) {
-    return p.cdr().eval(Context::make(p.car(), new Evaluator(a), a));
+    return new CurrentContext(p.car(), p.cdr(), a);
 }
 
 Ptr MTry::apply(const Ptr &p, const Ptr &a) {
