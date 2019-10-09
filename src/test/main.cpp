561197ef8eacff438d3b81718c16f7ea61dd0c17
Vasiliy <alfertev2012@gmail.com>
2019-10-09 15:57:40 +0300

    Add filname parameter for tests.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -10,12 +10,20 @@
 using namespace stepone::init;
 using namespace stepone::parser;
 
-int main() {
+int main(int argc, char *argv[]) {
     try {
         FirstParser fp(BaseEvaluator::createContext(), BaseSymbolTable::createSymbolTable());
 
+	std::string filename;
+        if (argc >= 2) {
+            filename.append(argv[1]);
+        }
+	else {
+            filename.append("./other/test.txt");
+        }
+
         TestEngine tfp(fp);
-        tfp.test_from_file("./other/test.txt"); // hardcoded!!!
+        tfp.test_from_file(filename);
 
         return 0;
     } catch (const std::exception& e) {
