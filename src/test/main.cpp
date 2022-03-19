cd4a11d95840736e9980f1cee429559f57d5e9ac
Vasiliy Alfertev <vasiliy.alfertev@corp.codeinside.ru>
2022-03-19 11:17:51 +0300

    Separate "init" sublibrary. Try run tests.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,10 +1,21 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <sstream>
+
+#include <firstparser.h>
+#include <initevaluator.h>
+#include <initsymbols.h>
+
+using namespace stepone::parser;
+using namespace stepone::init;
+
+TEST(FirstParserTest, Parcing) {
+    FirstParser fp(BaseEvaluator::createContext(), BaseSymbolTable::createSymbolTable());
+    auto nil = fp.parse("()");
+    std::stringstream ss;
+    fp.print(ss, nil);
+    auto str = ss.str();
+
+    EXPECT_EQ(str, "()");
 }
 
